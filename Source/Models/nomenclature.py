a2b49de3aad8f43e02fe131e4ab1151e8194c90b
AItEKS <egorka.ku06@mail.ru>
2024-02-13 18:07:10 +0800

    Reformat

--- Source/Models/nomenclature.py
+++ Source/Models/nomenclature.py
@@ -5,27 +5,33 @@ class nomenclature(abstract_reference):
     __group = None
     __unit = None
     __full_name = ""
-    
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.__group = ""
+        self.__unit = ""
+        self.__full_name = ""
+
     @property
     def group(self):
         return self.__group
-    
+
     @group.setter
     def group(self, value: abstract_reference):
         if value == "":
             self.error.set_error_source("Некорректно указана группа", self)
-            
+
         self.__group = value
-    
+
     @property
     def unit(self):
         return self.__unit
-    
+
     @unit.setter
     def unit(self, value: abstract_reference):
         if value == "":
             self.error.set_error_source("Некорректно указана единица измерения", self)
-            
+
         self.__unit = value
 
     @property

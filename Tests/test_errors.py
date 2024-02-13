b871dd7a5dc9f77359c0befc272fa41d1cc44433
AItEKS <egorka.ku06@mail.ru>
2024-02-13 16:10:14 +0800

    Reformat

--- Tests/test_errors.py
+++ Tests/test_errors.py
@@ -3,12 +3,10 @@ import unittest
 from Source.argument_exception import argument_exception
 
 
-
 class test_errors(unittest.TestCase):
-    
     def test_check_argument_exception(self):
         # Подготовка
-        
+
         # Действие
         try:
             raise argument_exception("Test")
@@ -16,34 +14,30 @@ class test_errors(unittest.TestCase):
             # Проверка
             print(ex.error.error_text)
             print(ex.error.error_source)
-            
-            
+
             assert ex.error.is_error
             return
-            
-        assert 1 != 1    
-        
-    
+
+        assert 1 != 1
+
     def test_check_set_exception(self):
         # Подготовка
         error = error_proxy()
-              
-        
+
         # Действие
         try:
             result = 1 / 0
-        except Exception as ex  :
+        except Exception as ex:
             error.set_error(ex)
-            
+
         # Проверки
         assert error.is_error == True
-        
-    
+
     def test_check_set_error_text(self):
         # Подготовка
         error = error_proxy("Test", "Test")
-        
+
         # Действие
-        
+
         # Проверки
         assert error.is_error == True

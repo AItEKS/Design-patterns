5b0c8e4ac13a589e4f12fa2ffe5d03495962753f
AItEKS <egorka.ku06@mail.ru>
2024-02-14 21:31:03 +0800

    Reformat

--- Source/operation_exception.py
+++ Source/operation_exception.py
@@ -1,13 +1,6 @@
-from error_proxy import error_proxy
+from Source.argument_exception import argument_exception
 
 
-class operation_exception(Exception):
-    __inner_error: error_proxy = error_proxy()
-
+class operation_exception(argument_exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
-        self.__inner_error.set_error(self)
-
-    @property
-    def error(self):
-        return self.__inner_error

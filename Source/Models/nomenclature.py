0ff741eecacbc94dff1b0ac5519e0e17114c5ca9
AItEKS <egorka.ku06@mail.ru>
2024-02-17 15:53:35 +0800

    Reformat

--- Source/Models/nomenclature.py
+++ Source/Models/nomenclature.py
@@ -2,12 +2,14 @@ from Source.abstract_reference import abstract_reference
 
 
 class nomenclature_model(abstract_reference):
-    def __init__(self, name):
-        super().__init__(name)
+    # Инициализация объекта
+    def init(self, name):
+        super().init(name)
         self.__group = None
         self.__unit = None
         self.__full_name = ""
 
+    # Геттер и сеттер для поля group
     @property
     def group(self):
         return self.__group
@@ -16,9 +18,9 @@ class nomenclature_model(abstract_reference):
     def group(self, value: abstract_reference):
         if value != None:
             self.error.set_error_source("Некорректно указана группа!", self)
-
         self.__group = value
 
+    # Геттер и сеттер для поля unit
     @property
     def unit(self):
         return self.__unit
@@ -27,9 +29,9 @@ class nomenclature_model(abstract_reference):
     def unit(self, value: abstract_reference):
         if value != None:
             self.error.set_error_source("Некорректно указана единица измерения!", self)
-
         self.__unit = value
 
+    # Геттер и сеттер для поля full_name
     @property
     def full_name(self):
         return self.__full_name
@@ -38,5 +40,4 @@ class nomenclature_model(abstract_reference):
     def full_name(self, value: str):
         if len(value) > 255:
             self.error.set_error_source("Превышена максимальная длина для полного наименования!", self)
-
         self.__full_name = value

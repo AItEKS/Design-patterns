b27875d0591b73ecc31b9dfa6f5fd0b7f4bc4af2
AItEKS <egorka.ku06@mail.ru>
2024-02-23 19:14:36 +0800

    add

--- Source/Models/storage.py
+++ Source/Models/storage.py
@@ -1,8 +1,8 @@
 from Source.abstract_reference import abstract_reference
-
+from unit import unit_model
 
 class storage_model(abstract_reference):
-    def __init__(self, name):
+    def init(self, name):
         super().__init__(name)
         self.name = name
         self.ingredients = {}
@@ -16,3 +16,25 @@ class storage_model(abstract_reference):
 
     def add_recipe(self, name, ingredients_list):
         self.recipes[name] = ingredients_list
+
+    @staticmethod
+    def create_unit_kilogram():
+        base = unit_model.create_unit_gramm()
+        item = unit_model('килограмм', base, 1000)
+        return item
+
+    @staticmethod
+    def create_unit_millilitr():
+        item = unit_model('миллилитр', None, 1)
+        return item
+
+    @staticmethod
+    def create_unit_litr():
+        base = storage_model.create_unit_millilitr()
+        item = unit_model('литр', base, 1000)
+        return item
+
+    @staticmethod
+    def create_unit_shtuki():
+        item = unit_model('штука', None, 1)
+        return item

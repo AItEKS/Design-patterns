7af74bb72df913ea9fc618990fc984c08b8b50dc
AItEKS <egorka.ku06@mail.ru>
2024-03-13 07:07:33 +0800

    New

--- Source/Logic/process_factory.py
+++ Source/Logic/process_factory.py
@@ -1,12 +1,28 @@
 from Source.Logic.process_storage_turn import process_storage_turn
-from Source.exceptions import operation_exception
+from Source.exceptions import operation_exception, exception_proxy
 
 
 class process_factory:
-    def create_process(self, process_type: str):
-        if process_type == 'storage_turn':
-            return process_storage_turn()
+    __maps = {}
 
-        else:
-            raise operation_exception("Неподдерживаемый тип процесса")
+    def __init__(self):
+        self.__build_structure()
+
+    def __build_structure(self):
+        self.__maps["storage_turn"] = process_storage_turn
+
+    def create(self, process_type: str, transactions: list):
+        exception_proxy.validate(process_type, str)
+        exception_proxy.validate(transactions, list)
+
+        if not transactions:
+            raise operation_exception("Список транзакций пуст")
+
+        if process_type not in self.__maps.keys():
+            raise operation_exception(f"Тип процесса {process_type} не поддерживается")
+
+        process_class = self.__maps[process_type]
+        result = process_class.process_storage_turn(transactions)
+
+        return result
 
